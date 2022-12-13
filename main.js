ed23427c37baa93aba575eb2a969e685e86938ec
Jhoan Garcia <jegarcia155@misena.edu.co>
2022-12-12 21:10:59 -0500

    Punto 5

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
-addEventListener("DOMContentLoaded", (e) => {
-    let myForm = document.querySelector("form")
-    let conver = document.querySelector("span")
-    let select = document.querySelector("#selection")
-    
-    
-    
-    select.addEventListener("click", (e) => {
-        let input = document.querySelector("#val")
-        if (e.target.value == "fahr") {
-            input.placeholder = "Ingrese grados Fahrenheit"
-        }
-        else if (e.target.value == "cen") {
-            input.placeholder = "Ingrese Grados Centigrados"
-        }
-    })
+addEventListener("DOMContentLoaded", (e)=>{
+    let form=document.querySelector('#form')
 
-    myForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let dataInput = Object.fromEntries(new FormData(e.target));
 
-        if (dataInput.conver == "fahr") {
-            let faC = Number(dataInput.valor - 32) * 5/9
-            conver.innerHTML = faC + ' grados Centigrados'
-        }
-
-        else if (dataInput.conver == "cen") {
-            let caF = Number(dataInput.valor * 9/5) + 32 
-            conver.innerHTML = caF + ' grados Fahrenheit'
-
-        }
-    })
-})
+    let cont = 1
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();    
+        if(cont <= 40){
+            let dataInput = Object.fromEntries(new FormData(e.target))
+            let fecha = dataInput.fecha
+            let plan = dataInput.planes
+            let tabla=document.querySelector("tbody");
+            tabla.insertAdjacentHTML("beforeend",`
+                <tr>
+                    <td> ${cont} </td>
+                    <td> ${fecha}  </td>
+                    <td> ${plan} </td>
+                </tr>
+                `)
+                cont++
+            }
+        })        
+    })         
